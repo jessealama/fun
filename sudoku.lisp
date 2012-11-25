d3d81058f1a6ddaad617b3ceaa92eb5f9fe9d11d
Jesse Alama <jesse.alama@gmail.com>
2012-11-25 18:47:26 +0100

    Refactoring.

--- sudoku.lisp
+++ sudoku.lisp
@@ -5,32 +5,25 @@
       (or (member first rest :test #'=)
 	  (contains-duplicates? rest)))))
 
+(defun sort-< (list)
+  (sort list #'<))
+
 (defun possibilities (target num-squares)
-  (if (< target 1)
-      nil
-      (if (< num-squares 1)
-	  nil
-	  (if (= num-squares 1)
-	      (if (< target 10)
-		  (list (list target))
-		  nil)
-	      (loop
-		 with more = nil
-		 for i from 1 upto 9
-		 do
-		   (let ((smaller (possibilities (- target i)
-						 (1- num-squares))))
-		     (let ((candidates (mapcar #'(lambda (possibility)
-						   (append (list i)
-							   possibility))
-					       smaller)))
-		       ;; (break "candidates = ~a" candidates)
-		       (let ((trimmed (remove-if #'contains-duplicates?
+  (cond ((< target 1) nil)
+	((< num-squares 1) nil)
+	((= num-squares 1) (when (< target 10) (list (list target))))
+	(t
+	 (loop
+	    with more = nil
+	    for i from 1 upto 9
+	    for smaller = (possibilities (- target i) (1- num-squares))
+	    for candidates = (mapcar #'(lambda (possibility)
+					 (append (list i)
+						 possibility))
+				     smaller)
+	    do
+	      (setf more (append more (remove-if #'contains-duplicates?
 						 candidates)))
-			 (setf more (append more trimmed)))))
-		 finally
-		   (setf more (mapcar #'(lambda (possibility)
-					  (sort possibility #'<))
-				      more))
-		   (setf more (remove-duplicates more :test #'equalp))
-		   (return more))))))
+	    finally
+	      (return (remove-duplicates (mapcar #'sort-< more)
+					 :test #'equalp))))))
